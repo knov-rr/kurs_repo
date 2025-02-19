c087b597c11d8b58e4e4476bcbea3396a224d351
knov-rr <knov@riksrevisjonen.no>
2025-02-19 13:50:32 +0100

    testest

--- script-knov.R
+++ script-knov.R
@@ -1,3 +1,6 @@
 # Scriptidiptidoo
 
 # Toodeloo
+
+Spørsmål: Hva hvis koden min avhenger av kode jeg ikke endrer, men som noen andre har endret? 
+Da blir det vel ingen merge-konflikt, men det kan vel bli problemer det er vanskelig å oppdage?
