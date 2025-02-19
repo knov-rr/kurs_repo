aea777e17c84ab5e9ad7b7f4ed459c01337a0fb9
knov-rr <knov@riksrevisjonen.no>
2025-02-19 13:53:19 +0100

    Branch commit

--- script-knov.R
+++ script-knov.R
@@ -4,3 +4,5 @@
 
 Spørsmål: Hva hvis koden min avhenger av kode jeg ikke endrer, men som noen andre har endret? 
 Da blir det vel ingen merge-konflikt, men det kan vel bli problemer det er vanskelig å oppdage?
+
+# Dette er en endring for test branchen
